f65c0c110da93d2b2177c8d67b4f67e9c8399040
Jonah Feinberg <174832162+jonahfeinberg@users.noreply.github.com>
2025-12-01 19:01:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ window.onmousemove = e => {
     if (track.dataset.mouseDownAt === "0") return;
 
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
-        maxDelta = window.innerWidth / 5;
+        maxDelta = window.innerWidth / 2;
 
     const percentage = (mouseDelta / maxDelta) * -100;
         nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
@@ -33,4 +33,5 @@ window.onmousemove = e => {
     }, { duration: 1000, fill: "forwards" });
 
 }
+
 
